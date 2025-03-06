7c3c95c5ac8d867a8fd2a427ed2743f4936f58a9
than <thanosz@outlook.com>
2025-03-06 14:40:52 +0200

    initial commit

---
+++ cp4i-mq-test.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+rm *.key *.srl *.crt *.csr *.p12
+openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:4096 -out ca.key
+openssl req -x509 -new -nodes -key ca.key -sha512 -days 30 -subj "/CN=example-selfsigned-ca" -out ca.crt
+openssl req -new -nodes -out example-qm.csr -newkey rsa:4096 -keyout example-qm.key -subj '/CN=example-qm'
+openssl x509 -req -in example-qm.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out example-qm.crt -days 3650 -sha512
+
+oc delete secret example-qm-tls
+oc create secret generic example-qm-tls --type="kubernetes.io/tls" --from-file=tls.key=example-qm.key --from-file=tls.crt=example-qm.crt --from-file=ca.crt
+
+openssl req -new -nodes -out example-app1.csr -newkey rsa:4096 -keyout example-app1.key -subj '/CN=example-app1'
+openssl x509 -req -in example-app1.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out example-app1.crt -days 3650 -sha512
+openssl pkcs12 -export -in "example-app1.crt" -name "example-app1" -certfile "ca.crt" -inkey "example-app1.key" -out "example-app1.p12" -passout pass:PASSWORD
+cat example-app1.crt ca.crt > example-app1-chain.crt
+
+oc delete cm example-tls-configmap
+oc apply  -f - << EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: example-tls-configmap
+data:
+  example-tls.mqsc: |
+    DEFINE CHANNEL('MTLS.SVRCONN') CHLTYPE(SVRCONN) SSLCAUTH(REQUIRED) SSLCIPH('ANY_TLS13_OR_HIGHER') REPLACE
+    SET CHLAUTH('MTLS.SVRCONN') TYPE(SSLPEERMAP) SSLPEER('CN=*') USERSRC(NOACCESS) ACTION(REPLACE)
+    SET CHLAUTH('MTLS.SVRCONN') TYPE(SSLPEERMAP) SSLPEER('CN=example-app1') USERSRC(MAP) MCAUSER('app1') ACTION(REPLACE)
+    SET AUTHREC PRINCIPAL('app1') OBJTYPE(QMGR) AUTHADD(CONNECT,INQ)
+    DEFINE QLOCAL('EXAMPLE.QUEUE') REPLACE
+    SET AUTHREC PROFILE('EXAMPLE.QUEUE') PRINCIPAL('app1') OBJTYPE(QUEUE) AUTHADD(BROWSE,PUT,GET,INQ)
+  example-tls.ini: |
+    Service:
+        Name=AuthorizationService
+        EntryPoints=14
+        SecurityPolicy=UserExternal
+EOF
+
+
+oc delete QueueManager exampleqm
+sleep 5
+oc create -f - << EOF
+apiVersion: mq.ibm.com/v1beta1
+kind: QueueManager
+metadata:
+  name: exampleqm
+spec:
+  license:
+    accept: true  
+    license: L-QYVA-B365MB
+    use: Production
+  queueManager:
+    name: EXAMPLEQM
+    mqsc:
+    - configMap:
+        name: example-tls-configmap
+        items:
+        - example-tls.mqsc
+    ini:
+    - configMap:
+        name: example-tls-configmap
+        items:
+        - example-tls.ini
+    storage:
+      queueManager:
+        type: ephemeral
+  version: 9.4.1.1-r1
+  web:
+    enabled: true
+  pki:
+    keys:
+      - name: default
+        secret:
+          secretName: example-qm-tls
+          items:
+            - tls.key
+            - tls.crt
+            - ca.crt
+EOF
+while true; do
+        HOSTNAME=$(oc get route exampleqm-ibm-mq-qm --template="{{.spec.host}}")
+        if [[ $? -ne 0 ]]; then 
+               sleep 5
+        else
+                break
+        fi
+done
+echo $HOSTNAME
+
+cat << EOF > ccdt.json
+{
+    "channel":
+    [
+        {
+            "name": "MTLS.SVRCONN",
+            "clientConnection":
+            {
+                "connection":
+                [
+                    {
+                        "host": "$HOSTNAME",
+                        "port": 443
+                    }
+                ],
+                "queueManager": "EXAMPLEQM"
+            },
+            "transmissionSecurity":
+            {
+              "cipherSpecification": "ANY_TLS13",
+              "certificateLabel": "example-app1"
+            },
+            "type": "clientConnection"
+        }
+   ]
+}
+EOF
+
+
+cat << EOF > mqclient.ini 
+Channels:
+  ChannelDefinitionDirectory=.
+  ChannelDefinitionFile=ccdt.json
+SSL:
+  OutboundSNI=HOSTNAME
+  SSLKeyRepository=example-app1.p12
+  SSLKeyRepositoryPassword=PASSWORD 
+EOF
+
+
+echo RUN:
+echo -e export MQSSLTRUSTSTORE=example-app1-chain.crt\\n/opt/mqm/samp/bin/amqsputc EXAMPLE.QUEUE EXAMPLEQM
